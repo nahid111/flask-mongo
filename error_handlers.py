0bbcf43300def8dc525f7f84c81a45e35c23a512
Muhammad Nahid <mdnahid22@gmail.com>
2020-08-19 12:59:40 +0600

    updated error handler

--- error_handlers.py
+++ error_handlers.py
@@ -2,12 +2,16 @@ def error_handler(app):
 
     @app.errorhandler(Exception)
     def handle_exception(e):
+        if e.__class__.__name__ == 'NotFound':
+            return {'success': False, 'error': 'Not Found'}, 404
         if e.__class__.__name__ == 'DoesNotExist':
             return {'success': False, 'error': 'Data Not Found'}, 404
+        if e.__class__.__name__ == 'ValidationError':
+            return {'success': False, 'error': str(e)}, 400
         if e.__class__.__name__ == 'NotUniqueError':
-            return {'success': False, 'error': 'Duplicate Field Value Entered'}, 401
+            return {'success': False, 'error': 'Duplicate Field Value Entered'}, 400
         if e.__class__.__name__ == 'KeyError':
-            return {'success': False, 'error': str(e) + ' field is missing'}, 401
+            return {'success': False, 'error': str(e) + ' field is missing'}, 400
 
         print('\x1b[91m' + e.__class__.__name__ + ': ' + str(e) + '\x1b[0m')
         return {'success': False, 'error': 'Something Went Wrong'}, 500
